939ecfcffd28a6d8e8f315b45f86a93360290152
Jerrod Linderman <nvmaxx@gmail.com>
2022-08-17 08:29:18 -0500

    Added questions to index.js for Manager, Engineer and Intern

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+
+
+const mQuestions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is your manager's name?"
+    },
+    {  
+        type: "input",
+        name: "managerId",
+        message: "What is your manager's id?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email?"
+    },
+];
+
+const eQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?"
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's id?"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?"
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your engineer's github username?"
+    }
+];
+
+const iQuestions = [
+    {
+        type: "input",
+        name: "internName",
+        message: "What is your intern's name?"
+    },
+    {
+        type: "input",
+        name: "internId",
+        message: "What is your intern's id?"
+    },
+    {
+        type: "input",
+        name: "internEmail",
+        message: "What is your intern's email?"
+    },
+    {
+        type: "input",
+        name: "internSchool",
+        message: "What is your intern's school?"
+    }   
+];
+
